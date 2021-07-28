bf1095a302575e52e893d168beba2fe24e6c41f3
Yean Seyha <yeanseyha7@gmail.com>
2021-07-28 10:44:04 +0700

    Add button components

--- frontend/components/Students.vue
+++ frontend/components/Students.vue
@@ -1,26 +1,35 @@
 <template>
-<div>
-    <h1>List Students</h1>
-    <table>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
         <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Action</th>
+          <th class="text-right">
+            Name
+          </th>
+          <th class="text-left">
+            Email
+          </th>
+          <th class="text-left">
+            Phone
+          </th>
+          <th class="text-left">
+            Action
+          </th>
         </tr>
-        <tr >
-            <td>Student1</td>
-            <td>Email1</td>
-            <td>Phone1</td>
-            <td>
-            <button>Update</button>
-            <button>Delete</button>
-            </td>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>data</td>
+          <td>
+              <v-btn color="red" elevation="11" small>Delete</v-btn>
+              <v-btn color="yellow" elevation="11" small>Update</v-btn>
+          </td>
         </tr>
-    </table>
-</div>
-    
-    
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script>
